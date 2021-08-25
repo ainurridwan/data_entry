c3a919613b0c7a706a0a99aeaf554dd6448e18e1
ainurridwan <ainurridwank2@gmail.com>
2021-08-25 14:56:28 +0700

    rewrite conflict file

--- modules/db_connection.php
+++ modules/db_connection.php
@@ -6,16 +6,9 @@ $dbName = 'db_data_entry';
 
 $con = mysqli_connect($server, $user , $password , $dbName);
 
-<<<<<<< HEAD
+
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL" . mysqli_connect_errno();
 } else {
-    // echo "database connected";
+    echo "database connected";
 }
-=======
-// if(mysqli_connect_errno()){
-//     echo "Failed to connect to MySQL" . mysqli_connect_errno();
-// } else {
-//     // echo "database connected";
-// }
->>>>>>> c78653dc5afef551bd9c9e4b7e2443a6e3bbdc78
